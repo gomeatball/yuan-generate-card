1f203e6a4815b9521682c773b607ef5ed5446167
Yuan Ma <mayuan131522@gmail.com>
2025-08-22 21:59:13 +0000

    main

--- src/app.js
+++ src/app.js
@@ -6,6 +6,50 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.getElementById("draw").addEventListener("click", function () {
+  let arrNumber = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let arrSign = ['♠', '♥', '♦', '♣'];
+
+
+
+  let randomCardLine = document.getElementById("randomCards");
+  let cardCount = document.getElementById("cardCount");
+  let desireCardCount = parseInt(cardCount.value);
+  console.log(desireCardCount);
+  
+  randomCardLine.innerHTML = "";
+
+ 
+  for (let i = 0; i < desireCardCount; i++) {
+
+  const randomCardNumberIndex = Math.floor(Math.random()*arrNumber.length);
+  const randomCardSignIndex= Math.floor(Math.random() * arrSign.length);
+  // let cardValue = arrNumber[randomCardNumberIndex] + arrSign[randomCardSignIndex];
+  let Element = document.createElement("div");
+  Element.innerHTML = `
+  <div class="card">
+    <span class="top">${arrSign[randomCardSignIndex]}</span>
+    <span class="center">${arrNumber[randomCardNumberIndex]}</span>
+    <span class="bottom">${arrSign[randomCardSignIndex]}</span>
+  </div>
+`;
+randomCardLine.appendChild(Element);
+let cardDiv = Element.querySelector(".card");
+
+if (arrSign[randomCardSignIndex] === "♥" || arrSign[randomCardSignIndex] === "♦") {
+  cardDiv.classList.add("red");
+}
+
+  }
+
+
+ 
+
+
+ 
+ 
+
+  });
+}
